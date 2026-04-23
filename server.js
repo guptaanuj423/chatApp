66c562c2d37c2788a90c8f4a8cdfcdf9b44f58a5
Anuj Gupta <anuj@grahaak.com>
2026-04-23 12:34:46 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("sendMessage", (data) => {
+    io.emit("receiveMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running xyz 123");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
