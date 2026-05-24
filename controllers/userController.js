312daad89fd3c5a525152f4ee6318103e0b45b7b
Anuj_1902 <anuj.gupta@dataman.in>
2026-05-24 22:47:15 +0530

    merged

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,15 +3,15 @@ const pool = require("../config/db");
 const getUsers = async (req, res) => {
   try {
     const result = await pool.query(
-        "SELECT id, username FROM userMaster ORDER BY id ASC"
+        "SELECT id,username FROM userMaster ORDER BY id ASC"
     );
-npmi
+
     res.json(result.rows);
 
   } catch (err) {
     console.log(err);
     res.status(500).json({
-      error: "Server error"
+      error: "Server error"+err
     });
   }
 };
